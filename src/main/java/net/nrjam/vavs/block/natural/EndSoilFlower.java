ee5c06ddfb51557f033cf99beb404a2dd68edfea
NumberJamie <jamievanessen@gmail.com>
2024-04-26 16:25:34 +0200

    Improve generation (#57)

--- src/main/java/net/nrjam/vavs/block/natural/EndSoilFlower.java
+++ src/main/java/net/nrjam/vavs/block/natural/EndSoilFlower.java
@@ -6,7 +6,6 @@ import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.FlowerBlock;
 import net.minecraft.world.level.block.state.BlockState;
-import net.nrjam.vavs.block.ModBlocks;
 import net.nrjam.vavs.datagen.tags.ModTags;
 import org.jetbrains.annotations.NotNull;
 
@@ -18,9 +17,9 @@ public class EndSoilFlower extends FlowerBlock {
     }
 
     @Override
-    public boolean canSurvive(BlockState state, @NotNull LevelReader lvl, @NotNull BlockPos pos) {
-        if (state.getBlock() == this) return lvl.getBlockState(pos).is(ModBlocks.END_SOIL.get());
-        return this.mayPlaceOn(lvl.getBlockState(pos), lvl, pos);
+    public boolean canSurvive(@NotNull BlockState state, @NotNull LevelReader lvl, @NotNull BlockPos pos) {
+        BlockPos blockPos = pos.below();
+        return this.mayPlaceOn(lvl.getBlockState(blockPos), lvl, blockPos);
     }
 
     @Override
